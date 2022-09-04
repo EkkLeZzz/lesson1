c2b7011e211f17572f1eb0b590c79be048975fa0
Andrey Artemov <felix.brookman@gmail.com>
2022-09-04 15:49:47 +0300

    my tasks

--- func1.py
+++ func1.py
@@ -2,4 +2,4 @@ def get_summ(one, two, delimiter='&'):
     summa_strok = str(one) + delimiter.upper() + str(two)
     print(summa_strok)
 
-get_summ("Андрей".upper(), "Александра".upper())
+get_summ("learn".upper(), "python".upper())
